45a65bb7acc9039be0e0d2174ca02913fa555da8
Sakebi <ilmabmail@gmail.com>
2017-08-03 20:03:03 +0200

    Update AssemblyInfo.cs

--- Sincerely-Yi/Properties/AssemblyInfo.cs
+++ Sincerely-Yi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sincerely Yi")]
+[assembly: AssemblyTitle("Sincerly Yi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sakebi")]
-[assembly: AssemblyProduct("Sincerely Yi")]
+[assembly: AssemblyProduct("Sincerly Yi")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
